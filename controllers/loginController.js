26fa7eb0a79f71e376759cac2e656e8dae322aaf
cWeber <cauna_weber@estudante.sesisenai.org.br>
2025-06-20 13:06:47 -0300

    dashboard template

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -33,10 +33,7 @@ exports.login = async (req, res) => {
       maxAge: 3600000 
     })
 
-    const message =
-      user.role === "admin" ? "Logado como ADM" : "Login realizado com sucesso";
-
-    res.render('login', {message: message, type: "success"})
+    res.redirect('dashboard')
 
   } catch (err) {
     res.status(500).json({ message: "erro ao logar", erro: err });
--- routes/router.js
+++ routes/router.js
@@ -6,6 +6,8 @@ const loginController = require('../controllers/loginController')
 const authMiddleware = require('../middlewares/authMiddleware');
 const helloWorld = require('../controllers/helloController')
 
+router.get('/', (req, res) => {res.redirect('/login')})
+
 router.post('/users', userController.createUser);
 router.get('/users', authMiddleware.authenticateToken, userController.getAllUser);
 router.get('/users/:id', authMiddleware.authenticateToken, userController.getUserById);
@@ -13,15 +15,14 @@ router.put('/users/:id', authMiddleware.authenticateToken, userController.update
 router.delete('/users/:id', authMiddleware.authenticateToken, authMiddleware.authorizeAdmin, userController.deleteUser);
 
 router.post('/login', loginController.login);
-router.get('/login', (req, res) => {
-  res.render('login', { message: null, type: null });
-});
+router.get('/login', (req, res) => {res.render('login', { message: null, type: null });});
+
 router.get('/logout', loginController.logout)
 
-router.get('/cadastro', (req, res) => {
-  res.render('cadastro', { message: null });
-});
+router.get('/cadastro', (req, res) => {res.render('cadastro', { message: null });});
 router.post('/cadastro', userController.createUser)
+
+router.get('/dashboard', authMiddleware.authenticateToken, (req, res) => {res.render('dashboard',{user: req.user})})
 
 router.get('/tokentest', authMiddleware.authenticateToken, helloWorld.helloWorld)
 router.get('/admtest', authMiddleware.authenticateToken, authMiddleware.authorizeAdmin, helloWorld.helloWorld)
