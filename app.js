e0035a09062ebf8b77b1329465676429d33eb601
cWeber <cauna_weber@estudante.sesisenai.org.br>
2025-06-21 20:48:30 -0300

    atualizacoes no css

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const sequelize = require("./config/database");
 const cookieParser = require('cookie-parser');
+const router = require("./routes/router");
 
 const app = express();
 app.use(express.json());
@@ -13,8 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {res.locals.currentUrl = req.originalUrl; next();});
 
-const router = require("./routes/router");
 app.use(router);
 
 sequelize.sync().then(() => {
